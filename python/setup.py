4e697012862fa82371dc8bd8ca908a808888b11f
Doug Shawhan <doug.shawhan@constructconnect.com>
2022-12-30 15:22:03 -0500

    reformatted with black for readability

--- python/setup.py
+++ python/setup.py
@@ -9,10 +9,12 @@ from shutil import copyfile, copytree, rmtree
 from setuptools.command.install import install
 
 try:
-    exec(open('version.py').read())
+    exec(open("version.py").read())
 except IOError:
-    print("Failed to load Zingg version file for packaging. You must be in Zingg's python dir.",
-          file=sys.stderr)
+    print(
+        "Failed to load Zingg version file for packaging. You must be in Zingg's python dir.",
+        file=sys.stderr,
+    )
     sys.exit(-1)
 
 try:
@@ -20,15 +22,17 @@ try:
     install_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(install_module)
 except IOError:
-    print("Failed to load the installing module (install.py) which had to be "
-          "packaged together.",
-          file=sys.stderr)
+    print(
+        "Failed to load the installing module (install.py) which had to be "
+        "packaged together.",
+        file=sys.stderr,
+    )
     sys.exit(-1)
 
-VERSION = __version__  
+VERSION = __version__
 ZINGG_HOME = os.path.abspath("../")
 
-print("zingg home is " , ZINGG_HOME)
+print("zingg home is ", ZINGG_HOME)
 
 # Provide guidance about how to use setup.py
 incorrect_invocation_message = """
@@ -49,7 +53,7 @@ JARS_PATH = glob.glob(os.path.join(ZINGG_HOME, "assembly/target"))
 if len(JARS_PATH) == 1:
     JARS_PATH = JARS_PATH[0]
 
-print("jar path is " , JARS_PATH)
+print("jar path is ", JARS_PATH)
 
 EXAMPLES_PATH = os.path.join(ZINGG_HOME, "examples")
 SCRIPTS_PATH = os.path.join(ZINGG_HOME, "scripts")
@@ -68,15 +72,15 @@ PHASES_TARGET = os.path.join("zingg", "phases")
 # This is important because we only want to build the symlink farm while under Zingg otherwise we
 # want to use the symlink farm. And if the symlink farm exists under while under Zingg (e.g. a
 # partially built sdist) we should error and have the user sort it out.
-in_zingg = (os.path.isfile("../core/src/main/java/zingg/Trainer.java") == 1)
+in_zingg = os.path.isfile("../core/src/main/java/zingg/Trainer.java") == 1
 
 
 def _supports_symlinks():
     """Check if the system supports symlinks (e.g. *nix) or not."""
     return getattr(os, "symlink", None) is not None
 
-class InstallCommand(install):
 
+class InstallCommand(install):
     def run(self):
         install.run(self)
 
@@ -93,6 +97,7 @@ class InstallCommand(install):
         #         dest=zingg_dist,
         #         zingg_version=zingg_version)
 
+
 try:
 
     try:
@@ -100,8 +105,8 @@ try:
     except OSError:
         # Don't worry if the directory already exists.
         pass
-    
-    if (in_zingg):
+
+    if in_zingg:
         # Construct the symlink farm - this is necessary since we can't refer to the path above the
         # package root and we need to copy the jars and scripts which are up above the python root.
         if _supports_symlinks():
@@ -122,8 +127,10 @@ try:
     else:
         # If we are not inside of ZINGG_HOME verify we have the required symlink farm
         if not os.path.exists(JARS_TARGET):
-            print("To build packaging must be in the python directory under the ZINGG_HOME.",
-                  file=sys.stderr)
+            print(
+                "To build packaging must be in the python directory under the ZINGG_HOME.",
+                file=sys.stderr,
+            )
 
     if not os.path.isdir(SCRIPTS_TARGET):
         print(incorrect_invocation_message, file=sys.stderr)
@@ -131,64 +138,62 @@ try:
 
     # Scripts directive requires a list of each script path and does not take wild cards.
     script_names = os.listdir(SCRIPTS_TARGET)
-    scripts = list(map(lambda script: os.path.join(SCRIPTS_TARGET, script), script_names))
-    
-    packages = []
-    packages.append('zingg')
-    #packages.append('zingg.pipes')
+    scripts = list(
+        map(lambda script: os.path.join(SCRIPTS_TARGET, script), script_names)
+    )
 
-    with open('README.md') as f:
+    packages = []
+    packages.append("zingg")
+    # packages.append('zingg.pipes')
+
+    with open("README.md") as f:
         long_description = f.read()
 
     setup(
-    name='zingg',  
-    version=VERSION,
-    author="Zingg.AI",
-    author_email="sonalgoyal4@gmail.com",
-    description="Zingg Entity Resolution, Data Mastering and Deduplication",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/zinggAI/zingg",
-    # packages=find_packages(),
-    packages=packages,
-    package_dir={
-            'zingg.jars': 'zingg/jars',
-            'zingg.scripts': 'zingg/scripts',
-            'zingg.data': 'zingg/models',
-            'zingg.examples': 'zingg/examples',
-            'zingg.conf': 'zingg/config',
-            'zingg.phases': 'zingg/phases'
+        name="zingg",
+        version=VERSION,
+        author="Zingg.AI",
+        author_email="sonalgoyal4@gmail.com",
+        description="Zingg Entity Resolution, Data Mastering and Deduplication",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/zinggAI/zingg",
+        # packages=find_packages(),
+        packages=packages,
+        package_dir={
+            "zingg.jars": "zingg/jars",
+            "zingg.scripts": "zingg/scripts",
+            "zingg.data": "zingg/models",
+            "zingg.examples": "zingg/examples",
+            "zingg.conf": "zingg/config",
+            "zingg.phases": "zingg/phases",
         },
         package_data={
-            'zingg.jars': ['*.jar'],
-            'zingg.scripts': ['*'],
-            'zingg.data': ['*'],
-            'zingg.examples': ['*.py', '*/examples/*.py'],
-            'zingg.conf': ['*'],
-            'zingg.phases': ['*'],
-            '':['*.py'],
-            '':['LICENCE']
-            },
+            "zingg.jars": ["*.jar"],
+            "zingg.scripts": ["*"],
+            "zingg.data": ["*"],
+            "zingg.examples": ["*.py", "*/examples/*.py"],
+            "zingg.conf": ["*"],
+            "zingg.phases": ["*"],
+            "": ["*.py"],
+            "": ["LICENCE"],
+        },
         include_package_data=True,
         scripts=scripts,
-        license='http://www.apache.org/licenses/LICENSE-2.0',
-        install_requires=['py4j==0.10.9'],
-        extras_require={
-            'zingg': ['pyspark>=3.1.2']
-        },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
-    ],
-    zip_safe = False, 
-    keywords="Entity Resolution, deduplication, record linkage, data mastering, identity resolution",
-    cmdclass={
-            'install': InstallCommand,
-        },
-)
+        license="http://www.apache.org/licenses/LICENSE-2.0",
+        install_requires=["py4j==0.10.9"],
+        extras_require={"zingg": ["pyspark>=3.1.2"]},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU Affero General Public License v3",
+            "Operating System :: OS Independent",
+        ],
+        zip_safe=False,
+        keywords="Entity Resolution, deduplication, record linkage, data mastering, identity resolution",
+        cmdclass={"install": InstallCommand,},
+    )
 finally:
-    if (in_zingg):
+    if in_zingg:
         # Depending on cleaning up the symlink farm or copied version
         if _supports_symlinks():
             os.remove(os.path.join("zingg", "jars"))
